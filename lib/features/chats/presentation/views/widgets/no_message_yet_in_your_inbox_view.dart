103e26682f50020e165193204cc67beef3a22349
Nidhal Khazene <nidhal.khazene@gmail.com>
2026-05-25 16:06:13 +0100

    no message yet in your inbox view ui done

--- lib/features/chats/presentation/views/widgets/no_message_yet_in_your_inbox_view.dart
+++ lib/features/chats/presentation/views/widgets/no_message_yet_in_your_inbox_view.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:home_space/core/utils/colors.dart';
+import 'package:home_space/core/utils/styles.dart';
+import 'package:home_space/shared/widgets/custom_button.dart';
+import 'package:home_space/shared/widgets/custom_oval_container.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 class NoMessageYetInYourInboxView extends StatelessWidget {
   const NoMessageYetInYourInboxView({super.key});
   static const String routeName = "no_message_yet_in_your_inbox_view";
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Placeholder());
+    return Scaffold(
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomOvalContainer(
+              width: 104,
+              height: 104,
+              backgroundColor: const Color(0xFFE8ECFF),
+              borderColor: Colors.transparent,
+              widget: Icon(
+                Iconsax.message_search_copy,
+                color: ColorsData.kPrimaryColor,
+                size: 64,
+              ),
+            ),
+            const SizedBox(height: 32),
+            SizedBox(
+              width: 250,
+              child: Text(
+                'No Message yet on your inbox',
+                textAlign: TextAlign.center,
+                style: AppStyles.medium24,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const CustomButton(width: 163, textContent: "Explore Listing"),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:home_space/features/chats/presentation/views/widgets/no_message_yet_in_your_inbox_view.dart';
 import 'package:home_space/shared/constants.dart';
 import 'package:home_space/core/routing/on_generate_route.dart';
 import 'package:home_space/core/services/shared_preferences_singleton.dart';
@@ -29,7 +30,7 @@ class HomeSpace extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       onGenerateRoute: onGenerateRoutes,
-      initialRoute: CustomBottomNavigationBar.routeName,
+      initialRoute: NoMessageYetInYourInboxView.routeName,
     );
   }
 }
