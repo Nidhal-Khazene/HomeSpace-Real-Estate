149b8bf26840b62fe65ed8472e21595bf487d43e
Nidhal Khazene <nidhal.khazene@gmail.com>
2026-05-26 17:29:35 +0100

    api service: implement rentcast api

--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -3,12 +3,23 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiService {
   final Dio _dio;
-  final String baseUrl = "https://api.rentcast.io/v1/listings/sale/";
-  final String apiKey = dotenv.get('RENTCAST_API_KEY');
-  ApiService(this._dio);
 
-  Future<Map<String, dynamic>> get({required String endPoint}) async {
-    var response = await _dio.get("$baseUrl$endPoint");
+  ApiService(this._dio) {
+    _dio.options = BaseOptions(
+      baseUrl: 'https://api.rentcast.io/v1',
+      headers: {
+        'X-Api-Key': dotenv.get('RENTCAST_API_KEY'),
+        'accept': 'application/json',
+      },
+    );
+  }
+
+  Future<dynamic> get({
+    required String endPoint,
+    Map<String, dynamic>? queryParameters,
+  }) async {
+    var response = await _dio.get(endPoint, queryParameters: queryParameters);
+
     return response.data;
   }
 }
