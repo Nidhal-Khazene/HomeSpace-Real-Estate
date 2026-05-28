40c1b453dbc75d3b6e48a03c98a800b28b6ad7f5
Nidhal Khazene <nidhal.khazene@gmail.com>
2026-05-28 14:25:01 +0100

    refactor sale listings entity

---
+++ lib/shared/use_cases/use_case.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:home_space/core/errors/failures.dart';
+
+abstract class UseCase<Type, Param> {
+  Future<Either<Failures, Type>> call([Param param]);
+}
+
+class NoParam {}
