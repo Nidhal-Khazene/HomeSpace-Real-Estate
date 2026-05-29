80fa2f3276a0460402499c0709596fd889db2cd1
Nidhal Khazene <nidhal.khazene@gmail.com>
2026-05-29 20:40:45 +0100

    implement get sale listings cubit

--- lib/features/home/presentation/manager/get_sale_listings_cubit/get_sale_listings_cubit.dart
+++ lib/features/home/presentation/manager/get_sale_listings_cubit/get_sale_listings_cubit.dart
@@ -1,9 +1,27 @@
 import 'package:bloc/bloc.dart';
 import 'package:home_space/features/home/domain/entities/sale_listings_entity.dart';
+import 'package:home_space/features/home/domain/use_cases/get_sale_listings_use_case.dart';
 import 'package:meta/meta.dart';
 
 part 'get_sale_listings_state.dart';
 
 class GetSaleListingsCubit extends Cubit<GetSaleListingsState> {
-  GetSaleListingsCubit() : super(GetSaleListingsInitial());
+  GetSaleListingsCubit({required this.getSaleListingsUseCase})
+    : super(GetSaleListingsInitial());
+  final GetSaleListingsUseCase getSaleListingsUseCase;
+  Future<dynamic> getSaleListings({int pageNumber = 0}) async {
+    if (pageNumber == 0) {
+      emit(GetSaleListingsLoading());
+    } else {
+      emit(GetSaleListingsPaginationLoading());
+    }
+    var result = await getSaleListingsUseCase.call(pageNumber);
+    result.fold((failure) {
+      if (pageNumber == 0) {
+        emit(GetSaleListingsFailure(errMessage: failure.errMessage));
+      } else {
+        emit(GetSaleListingsPaginationFailure(errMessage: failure.errMessage));
+      }
+    }, (listings) => emit(GetSaleListingsSuccess(listings: listings)));
+  }
 }
