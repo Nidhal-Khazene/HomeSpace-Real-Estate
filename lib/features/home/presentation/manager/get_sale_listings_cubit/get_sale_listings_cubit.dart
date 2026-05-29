6a79e5697c12a4a5267267e609dcd6c8165e8596
Nidhal Khazene <nidhal.khazene@gmail.com>
2026-05-29 20:33:11 +0100

    add get sale listings states

--- lib/features/home/presentation/manager/get_sale_listings_cubit/get_sale_listings_cubit.dart
+++ lib/features/home/presentation/manager/get_sale_listings_cubit/get_sale_listings_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:home_space/features/home/domain/entities/sale_listings_entity.dart';
 import 'package:meta/meta.dart';
 
 part 'get_sale_listings_state.dart';
--- lib/features/home/presentation/manager/get_sale_listings_cubit/get_sale_listings_state.dart
+++ lib/features/home/presentation/manager/get_sale_listings_cubit/get_sale_listings_state.dart
@@ -4,3 +4,26 @@ part of 'get_sale_listings_cubit.dart';
 sealed class GetSaleListingsState {}
 
 final class GetSaleListingsInitial extends GetSaleListingsState {}
+
+final class GetSaleListingsLoading extends GetSaleListingsState {}
+
+final class GetSaleListingsSuccess extends GetSaleListingsState {
+  final List<SaleListingsEntity> listings;
+
+  GetSaleListingsSuccess({required this.listings});
+}
+
+final class GetSaleListingsFailure extends GetSaleListingsState {
+  final String errMessage;
+
+  GetSaleListingsFailure({required this.errMessage});
+}
+
+// Pagination States
+final class GetSaleListingsPaginationFailure extends GetSaleListingsState {
+  final String errMessage;
+
+  GetSaleListingsPaginationFailure({required this.errMessage});
+}
+
+final class GetSaleListingsPaginationLoading extends GetSaleListingsState {}
