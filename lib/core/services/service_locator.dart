c0c9489b1fb58b4b5f86564841fe639c776cb1f9
Nidhal Khazene <nidhal.khazene@gmail.com>
2026-05-30 15:33:10 +0100

    service locator: add HomeRepo service locator

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,9 +1,21 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:home_space/Features/home/data/data_sources/home_local_data_source.dart';
+import 'package:home_space/Features/home/data/data_sources/home_remote_data_source.dart';
 import 'package:home_space/core/services/api_service.dart';
+import 'package:home_space/features/home/data/repos/home_repo_impl.dart';
+import 'package:home_space/features/home/domain/repos/home_repo.dart';
 
 final GetIt getIt = GetIt.instance;
 
 void serviceLocator() {
   getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  getIt.registerSingleton<HomeRepo>(
+    HomeRepoImpl(
+      homeLocalDataSource: HomeLocalDataSourceImpl(),
+      homeRemoteDataSource: HomeRemoteDataSourceImpl(
+        apiService: getIt.get<ApiService>(),
+      ),
+    ),
+  );
 }
