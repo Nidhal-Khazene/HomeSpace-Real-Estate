07069b0403d045b13a97385d3eff6217c72cf802
Nidhal Khazene <nidhal.khazene@gmail.com>
2026-07-29 21:05:14 +0100

    sale_listings_list_view_bloc_consumer: add the pagination states

--- lib/features/home/presentation/views/widgets/sale_listings_list_view_bloc_consumer.dart
+++ lib/features/home/presentation/views/widgets/sale_listings_list_view_bloc_consumer.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:home_space/features/home/domain/entities/sale_listings_entity.dart';
 import 'package:home_space/features/home/presentation/manager/get_sale_listings_cubit/get_sale_listings_cubit.dart';
 import 'package:home_space/shared/functions/get_dummy_sale_listings.dart';
-import 'package:home_space/shared/functions/show_failure_snack_bar.dart';
 import 'package:home_space/shared/widgets/custom_no_data_view.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 import 'sale_listings_list_view.dart';
@@ -18,31 +16,30 @@ class SaleListingsListViewBlocConsumer extends StatefulWidget {
 
 class _SaleListingsListViewBlocConsumerState
     extends State<SaleListingsListViewBlocConsumer> {
-  List<SaleListingsEntity> saleListings = [];
-
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<GetSaleListingsCubit, GetSaleListingsState>(
+      buildWhen: (previous, current) {
+        return current is GetSaleListingsLoading ||
+            current is GetSaleListingsSuccess ||
+            current is GetSaleListingsFailure;
+      },
+      listener: (context, state) {
+        if (state is GetSaleListingsPaginationFailure) {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text(state.errMessage)));
+        }
+      },
       builder: (context, state) {
-        if (state is GetSaleListingsSuccess ||
-            state is GetSaleListingsPaginationLoading ||
-            state is GetSaleListingsPaginationFailure) {
-          return SaleListingsListView(saleListings: saleListings);
-        } else if (state is GetSaleListingsFailure) {
-          return const CustomNoDataView();
-        } else {
+        if (state is GetSaleListingsLoading) {
           return Skeletonizer(
             child: SaleListingsListView(saleListings: getDummySaleListings()),
           );
-        }
-      },
-      listener: (context, state) {
-        if (state is GetSaleListingsSuccess) {
-          saleListings = state.listings;
-        }
-
-        if (state is GetSaleListingsPaginationFailure) {
-          showFailureSnackBar(context, message: state.errMessage);
+        } else if (state is GetSaleListingsSuccess) {
+          return SaleListingsListView(saleListings: state.listings);
+        } else {
+          return const CustomNoDataView();
         }
       },
     );
