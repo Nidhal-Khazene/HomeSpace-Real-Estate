6746fef9cacefe8ed05645a106d575c0ab76a466
Nidhal Khazene <nidhal.khazene@gmail.com>
2026-07-29 21:04:13 +0100

    home main section: add NotificationListener

--- lib/features/home/presentation/views/widgets/home_main_section.dart
+++ lib/features/home/presentation/views/widgets/home_main_section.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:home_space/features/home/presentation/manager/get_sale_listings_cubit/get_sale_listings_cubit.dart';
 import 'package:home_space/features/home/presentation/views/widgets/sale_listings_list_view_bloc_consumer.dart';
 
 import '../../../../../core/utils/styles.dart';
@@ -10,21 +12,31 @@ class HomeMainSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const CustomSearchBar(
-            textContent: "Search Address, City or Postal code",
-            state: true,
-          ),
-          const SizedBox(height: 32),
-          Text("Listings Nearby", style: AppStyles.medium16),
-          const SizedBox(height: 16),
-          const CategoryChips(),
-          const SizedBox(height: 16),
-          const SaleListingsListViewBlocConsumer(),
-        ],
+    return NotificationListener<ScrollUpdateNotification>(
+      onNotification: (notification) {
+        final pixels = notification.metrics.pixels;
+        final max = notification.metrics.maxScrollExtent;
+        if (pixels >= max * 0.7) {
+          context.read<GetSaleListingsCubit>().getSaleListings();
+        }
+        return false;
+      },
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const CustomSearchBar(
+              textContent: "Search Address, City or Postal code",
+              state: true,
+            ),
+            const SizedBox(height: 32),
+            Text("Listings Nearby", style: AppStyles.medium16),
+            const SizedBox(height: 16),
+            const CategoryChips(),
+            const SizedBox(height: 16),
+            const SaleListingsListViewBlocConsumer(),
+          ],
+        ),
       ),
     );
   }
