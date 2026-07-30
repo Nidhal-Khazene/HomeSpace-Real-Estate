aafc0b112529756c9f041be505ad7252e12c9b26
Nidhal Khazene <nidhal.khazene@gmail.com>
2026-07-30 08:59:16 +0100

    constants: add kApiPaginationLimit constant

--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -2,6 +2,7 @@ import 'package:home_space/core/services/api_service.dart';
 import 'package:home_space/core/services/backend_breakpoint.dart';
 import 'package:home_space/features/home/data/models/sale_listings_model.dart';
 import 'package:home_space/features/home/domain/entities/sale_listings_entity.dart';
+import 'package:home_space/shared/constants.dart';
 
 abstract class HomeRemoteDataSource {
   Future<List<SaleListingsEntity>> getSaleListings({int pageNumber = 0});
@@ -16,7 +17,7 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
   Future<List<SaleListingsEntity>> getSaleListings({int pageNumber = 0}) async {
     final data = await apiService.get(
       endPoint:
-          "${BackendBreakpoint.kGetSaleListings}?city=Austin&state=TX&status=Active&limit=10&offset=${pageNumber * 10}",
+          "${BackendBreakpoint.kGetSaleListings}?city=Austin&state=TX&status=Active&limit=$kApiPaginationLimit&offset=${pageNumber * 10}",
     );
     return getListings(data);
   }
--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -12,3 +12,5 @@ const int kMillisecondsDuration = 300;
 const String kIsOnBoardingViewSeen = "isOnBoardingViewSeen";
 const double kNormalRadiusChart = 30;
 const double kTouchedRadiusChart = 40;
+
+const int kApiPaginationLimit = 20;
